2c96a1a3702d2aa7d17a73aab866800db4a01a88
DEEPU KUMAR GUPTA <84670355+deepug12@users.noreply.github.com>
2022-01-03 00:37:57 +0530

    Update Prime or not.cpp

--- Prime or not.cpp
+++ Prime or not.cpp
@@ -1,16 +1,5 @@
-// Aggresive cows 
-/* Eko problem from Spoj */
 #include<bits/stdc++.h> 
 using namespace std;
-bool containsDuplicate(vector<int>& nums) {
-        int ct = 0;
-        for(int i = 0; i < nums.size(); i++){
-        	if(nums[i] == nums[i+1]) ct++;
-        }
-        if(ct >= 2) return true;
-        else return false;
-        
-    }
 int main() 
 {
 	int n;
