bc66a19bf8936d6637bd59a59cd39c73dd9cd4a7
DEEPU KUMAR GUPTA <84670355+deepug12@users.noreply.github.com>
2022-01-07 22:08:59 +0530

    Count Distnct Elements in an Array.
    Count All Distnct Elements in an Array.

---
+++ CountDistnct.cpp
@@ -0,0 +1,20 @@
+// Bubble Sort 
+
+#include<bits/stdc++.h> 
+using namespace std;
+int main() 
+{
+	set<int> s;
+	int n;
+	cin>>n;
+	for (int i = 0; i < n; ++i)
+	{
+		int x;
+		cin>>x;
+		s.insert(x);
+	}
+	cout<<s.size()<<'\n';
+
+    
+    return 0; 
+} 
