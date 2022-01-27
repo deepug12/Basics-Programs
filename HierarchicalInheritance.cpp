de7eb1075beee1beb496c96ec1e6355c355dbf1a
Deepu Gupta <84670355+deepug12@users.noreply.github.com>
2022-01-28 01:47:44 +0530

    Hierarchical Inheritance..
    Hierarchical Inheritance Implementation...

---
+++ HierarchicalInheritance.cpp
@@ -0,0 +1,46 @@
+// Hierarchical Inheritance Implementation.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class A{
+
+public:
+
+	void func1(){
+		cout << "Inside function 1"<<'\n';
+	}
+
+};
+class B : public A{
+public:
+	void func2(){
+		cout<<"Inside function 2"<<'\n';
+	}
+};
+class C : public A{
+public:
+	void func3(){
+		cout<<"Inside function 3"<<'\n';
+	}
+};
+int main(){
+
+	A obj1;
+	obj1.func1();
+
+	B obj2;
+	obj2.func1();
+	obj2.func2();
+
+	C obj3;
+
+	obj3.func1();
+	obj3.func3();
+
+
+	return 0;
+
+
+	
+}
